81e191cdf18d95c6729c29cd33f35abcecef0d2b
fsj2009yx <289137665@qq.com>
2024-11-21 15:39:19 +0800

    CORS fixed

--- start.cpp
+++ start.cpp
@@ -41,24 +41,51 @@ int main()
     CROW_ROUTE(app, "/avl").methods("GET"_method)
         ([&avl](const crow::request& req) {
             string keyword= req.url_params.get("keyword");
-            return getDict(avl, keyword);
+
+            crow::response res;
+            res.set_header("Access-Control-Allow-Origin", "*");
+            res.set_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            res.set_header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+            res.write(crow::json::dump(getDict(avl, keyword)));
+            return res;
             });
 
     CROW_ROUTE(app, "/bst").methods("GET"_method)
         ([&bst](const crow::request& req) {
             string keyword = req.url_params.get("keyword");
-            return getDict(bst, keyword);
+
+            crow::response res;
+            res.set_header("Access-Control-Allow-Origin", "*");
+            res.set_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            res.set_header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+            res.write(crow::json::dump(getDict(bst, keyword)));
+            return res;
             });
 
     CROW_ROUTE(app, "/binarysearch").methods("GET"_method)
         ([&array](const crow::request& req) {
         string keyword = req.url_params.get("keyword");
-        return getDict(array, keyword);
+
+        crow::response res;
+        res.set_header("Access-Control-Allow-Origin", "*");
+        res.set_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.set_header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+        res.write(crow::json::dump(getDict(array, keyword)));
+        return res;
         });
 
     CROW_ROUTE(app, "/map").methods("GET"_method)
         ([&mp](const crow::request& req) {
         string keyword = req.url_params.get("keyword");
+
+        crow::response res;
+        res.set_header("Access-Control-Allow-Origin", "*");
+        res.set_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.set_header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
         vector<pair<string, string>> dict =mp.search(keyword);
         crow::json::wvalue data;
         if (!keyword.length()) {
@@ -74,7 +101,8 @@ int main()
                 }
             }
         }
-        return data;
+        res.write(crow::json::dump(data));
+        return res;
         });
 
 
